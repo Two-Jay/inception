584477193ebbbb8a836ae54b2aedac834d8e1dce
Two-Jay <djeeee1272@gmail.com>
2022-11-10 22:31:25 +0900

    check

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 sudo apt-get update -y && apt-get upgrade -y;
 
 #git, vim, make 설치
-sudo apt-get install -y git vim make;
+sudo apt-get install -y vim make;
 
 bash ./srcs/init_docker.sh
-make up
--- srcs/wordpress/config/setup.sh
+++ srcs/wordpress/config/setup.sh
@@ -1,4 +1,3 @@
-unzip latest-ko_KR.zip -d /var/www/
-chown -R www-data:www-data /var/www/wordpress
-rm -rf /var/www/wordpress/wp-config-sample.php
-mkdir -p /run/php
+
+
+
