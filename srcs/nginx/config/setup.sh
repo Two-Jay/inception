ef4570b2fe43c6471d968d1c0495fd5f43e283f8
Two-Jay <djeeee1272@gmail.com>
2022-11-10 23:42:24 +0900

    check

--- srcs/nginx/config/setup.sh
+++ srcs/nginx/config/setup.sh
@@ -1,15 +1,10 @@
-openssl genrsa -out server.key 4096;
+;
 sleep 5
-openssl req -new -key server.key -subj "/C=KR/ST=Seoul/O=42seoul/OU=gam/CN=inception" -out server.csr;
+;
 sleep 5
-openssl req -x509 -days 365 -in server.csr -key server.key -out server.crt;
+;
 sleep 3
 
-chmod 666 server.crt server.key;
+;
 
-mkdir -p /etc/ssl/certs;
-mkdir -p /etc/ssl/private;
-
-mv server.crt /etc/ssl/certs;
-mv server.key /etc/ssl/private;
 
